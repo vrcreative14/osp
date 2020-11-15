1de53f5b569555215b937759a121b2c833d0b279
Kartikeya <vrcreative14@gmail.com>
2020-11-16 02:28:04 +0530

    changes in settings.py wrt TEMPLATES DIRS

--- OriginalStores/settings.py
+++ OriginalStores/settings.py
@@ -63,11 +63,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'OriginalStores.urls'
-
+PROJECT_DIR = os.path.dirname(__file__)
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
         'APP_DIRS': True,
         'OPTIONS': {    
             'context_processors': [
