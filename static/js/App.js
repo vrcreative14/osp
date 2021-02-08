7dbe1fe72da228a4f39cd500528dd376ea093e04
vrcreative14 <69114615+vrcreative14@users.noreply.github.com>
2021-02-08 20:01:18 +0530

    Update App.js
    Changed 127.0.0.1:8000 to www.vcnity.online

--- static/js/App.js
+++ static/js/App.js
@@ -84,7 +84,7 @@ function fetchStores() {
 var wrapper = document.getElementById('storeListDiv')
 //wrapper.innerHTML=''
  console.log('fetching....')
-  fetch('http://127.0.0.1:8000/api/store-list/', {
+  fetch('http://www.vcnity.online/api/store-list/', {
       method:'GET',              
   })
   .then(response => response.json())
